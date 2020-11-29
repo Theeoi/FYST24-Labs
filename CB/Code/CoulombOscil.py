835d988aba5c5149986ea979ed024b9297d9b7d6
Theodor Blom <theodor.blom1@gmail.com>
2020-11-29 18:03:13 +0100

    Finished plotting everything for the ColoumbOscillations and saved the images.

--- CB/Code/CoulombOscil.py
+++ CB/Code/CoulombOscil.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+import statistics
 
 # Read the data into list.
-data = open("../Data/group4_log1.txt",'r')
-dataLines = data.readlines()
+data1 = open("../Data/group4_log1.txt",'r')
+data1Lines = data1.readlines()
 
-voltage = []
-current = []
-for line in dataLines[2:]:
-    voltage.append(float(line.split("\t")[1]))
-    current.append(float(line.split("\t")[2]))
+voltage1 = []
+current1 = []
+for line in data1Lines[2:]:
+    voltage1.append(float(line.split("\t")[1]))
+    current1.append(float(line.split("\t")[2]))
 
-data.close()
-newCurrent = [i * 10**12 for i in current] #Multiplying current to read pA instead of A.
+data1.close()
+
+#ColoumbOscillation Zoomin
+
+data2 = open("../Data/group4_log2.txt",'r')
+data2Lines = data2.readlines()
+
+voltage2 = []
+current2_forward = []
+current2_backward = []
+for line in data2Lines[2:]:
+    voltage2.append(float(line.split("\t")[1]))
+    current2_forward.append(float(line.split("\t")[2]))
+    current2_backward.append(float(line.split("\t")[3]))
+
+data2.close()
+
+#Plotting Full Oscillation scan
 
 plt.figure(figsize = (12,8))
 
@@ -23,8 +40,28 @@ plt.ylabel("Tunneling Current [pA]", fontsize = "x-large")
 
 plt.text(2.3, 350, "$V_{SD}$ = 0.1 mV", fontsize = "x-large")
 
-plt.xlim(2, voltage[-1])
+plt.xlim(2, voltage1[-1])
 
-plt.plot(voltage,newCurrent)
-plt.show()
+plt.plot(voltage1,[i * 10**12 for i in current1])
+#plt.show()
+plt.savefig('../Fig/ColoumbOscillations_Full.png')
+
+#Plotting Oscillation Zoomin
+
+plt.figure(figsize = (12,8))
+
+plt.title("Coloumb Oscillations", fontsize = "xx-large")
+plt.xlabel("Gate Voltage [V]", fontsize = "x-large")
+plt.ylabel("Tunneling Current [pA]", fontsize = "x-large")
+
+plt.text(3.35, 30, "$V_{SD}$ = 0.05 mV", fontsize = "x-large")
+
+plt.xlim(voltage2[0], voltage2[-1])
+
+current2 = [statistics.mean(k) for k in zip(current2_forward, current2_backward)]
+plt.plot(voltage2,[i * 10**12 for i in current2])
+#plt.show()
+plt.savefig('../Fig/ColoumbOscillations_Zoom.png')
+
+
 
